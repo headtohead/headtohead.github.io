60ec14fbf268cb3ff3fc7388147fb44ca1e91941
jakemingolla <jakemingolla@gmail.com>
2019-10-27 15:44:20 -0400

    Update index.html

---
+++ build/precache-manifest.a2342155ceb4a6a293217bb9c5ffbda1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9927acd372215f1bb2fc25077583dc3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b70d95d971fbdf785a95",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "36aa04212e001735b081",
+    "url": "/static/js/2.ee23e378.chunk.js"
+  },
+  {
+    "revision": "b70d95d971fbdf785a95",
+    "url": "/static/js/main.d0ad3aed.chunk.js"
+  },
+  {
+    "revision": "e9d1739d2478d6afe80d",
+    "url": "/static/js/runtime-main.3ae7e092.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
