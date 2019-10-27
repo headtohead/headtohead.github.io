283308ab0367e1d5882a17569c9bd84f2a928926
jakemingolla <jakemingolla@gmail.com>
2019-10-27 16:22:51 -0400

    updates to source

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Hello jake and jake2 and jake3!
+          Edit <code>src/App.js</code> and save to reload. Hello jake and jake2 and jake3 and jake4!
         </p>
         <a
           className="App-link"
