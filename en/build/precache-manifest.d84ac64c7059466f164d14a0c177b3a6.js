8eb9e22764cd347ef11af2dd3fff9a31387a5cb9
jakemingolla <jakemingolla@gmail.com>
2019-10-27 16:17:40 -0400

    package.json changes

---
+++ en/build/precache-manifest.d84ac64c7059466f164d14a0c177b3a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5757931899d87c60c8d03187ba47d393",
+    "url": "/en/index.html"
+  },
+  {
+    "revision": "82619fa88f707954f4af",
+    "url": "/en/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "36aa04212e001735b081",
+    "url": "/en/static/js/2.ee23e378.chunk.js"
+  },
+  {
+    "revision": "82619fa88f707954f4af",
+    "url": "/en/static/js/main.cf2489f2.chunk.js"
+  },
+  {
+    "revision": "3a23ae9ba9eea171ed95",
+    "url": "/en/static/js/runtime-main.9e441e1d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/en/static/media/logo.25bf045c.svg"
+  }
+]);
