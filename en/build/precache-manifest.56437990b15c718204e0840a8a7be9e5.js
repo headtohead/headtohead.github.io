0900d45b14582116b17bd5f88bdac24f6c1c14eb
jakemingolla <jakemingolla@gmail.com>
2019-10-27 16:23:03 -0400

    Build 0.1.2

---
+++ en/build/precache-manifest.56437990b15c718204e0840a8a7be9e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c394e857bf4012d070d0efa11f5958b8",
+    "url": "/en/index.html"
+  },
+  {
+    "revision": "6a87d09ed8703ff61e16",
+    "url": "/en/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "36aa04212e001735b081",
+    "url": "/en/static/js/2.ee23e378.chunk.js"
+  },
+  {
+    "revision": "6a87d09ed8703ff61e16",
+    "url": "/en/static/js/main.4d3008d7.chunk.js"
+  },
+  {
+    "revision": "3a23ae9ba9eea171ed95",
+    "url": "/en/static/js/runtime-main.9e441e1d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/en/static/media/logo.25bf045c.svg"
+  }
+]);
