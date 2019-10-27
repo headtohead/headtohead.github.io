e60aef27d885f9c3ac895129f9e8342cbfeffa57
jakemingolla <jakemingolla@gmail.com>
2019-10-27 16:06:34 -0400

    Initial first commit ;)

---
+++ build/precache-manifest.1c5ecc7a4544ef2abe8e7d7a1ac96ce9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70f6aa0b26abae9460ac719111f50b07",
+    "url": "/build/index.html"
+  },
+  {
+    "revision": "7f9d82b97ba49e2f205a",
+    "url": "/build/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "36aa04212e001735b081",
+    "url": "/build/static/js/2.ee23e378.chunk.js"
+  },
+  {
+    "revision": "7f9d82b97ba49e2f205a",
+    "url": "/build/static/js/main.bcd77b71.chunk.js"
+  },
+  {
+    "revision": "47abd199ce4b1b3d4f01",
+    "url": "/build/static/js/runtime-main.ddd2d675.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/build/static/media/logo.25bf045c.svg"
+  }
+]);
